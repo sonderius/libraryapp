76a2f041f29abed94c00f488987122f1a1edb588
sonderius <lubomir.zavodsky@outlook.sk>
2023-10-26 16:01:26 +0200

    +add control login for action

--- controllers/AdministrationController.php
+++ controllers/AdministrationController.php
@@ -1,12 +1,16 @@
 <?php
-
+//služi na rozcestnik pre volbu administracie zaznamov v db
 namespace app\controllers;
+use Yii;
 
 class AdministrationController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        if (Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+        else return $this->render('\index');
     }
 
 }
