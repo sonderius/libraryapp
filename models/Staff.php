eda5b9fcaadadaccf4cbe2def47e169aaac1a9fa
sonderius <lubomir.zavodsky@outlook.sk>
2023-10-26 10:34:10 +0200

    +new model staff

--- models/Staff.php
+++ models/Staff.php
@@ -2,6 +2,7 @@
 
 namespace app\models;
 
+
 use Yii;
 
 /**
@@ -12,6 +13,12 @@ use Yii;
  * @property int $contact
  * @property int $salary
  * @property int $staff_number
+ * @property string $email
+ * @property int $id
+ * @property string $username
+ * @property string $password
+ * @property string $authKey
+ * @property string $accessToken
  *
  * @property Member[] $members
  */
@@ -31,9 +38,9 @@ class Staff extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['name', 'position', 'contact', 'salary', 'staff_number'], 'required'],
-            [['name', 'position'], 'string'],
-            [['contact', 'salary', 'staff_number'], 'integer'],
+            [['name', 'position', 'contact', 'salary', 'staff_number', 'email', 'id', 'username', 'password', 'authKey', 'accessToken'], 'required'],
+            [['name', 'position', 'email', 'username', 'password', 'authKey', 'accessToken'], 'string'],
+            [['contact', 'salary', 'staff_number', 'id'], 'integer'],
             [['staff_number'], 'unique'],
         ];
     }
@@ -49,6 +56,12 @@ class Staff extends \yii\db\ActiveRecord
             'contact' => 'Contact',
             'salary' => 'Salary',
             'staff_number' => 'Staff Number',
+            'email' => 'Email',
+            'id' => 'ID',
+            'username' => 'Username',
+            'password' => 'Password',
+            'authKey' => 'Auth Key',
+            'accessToken' => 'Access Token',
         ];
     }
 
@@ -60,5 +73,32 @@ class Staff extends \yii\db\ActiveRecord
     public function getMembers()
     {
         return $this->hasMany(Member::class, ['registrated_by' => 'staff_number']);
+    }
+
+    public function getAuth()
+    {
+        return "[
+            '100' => [
+                'id' => '100',
+                'username' => 'admin',
+                'password' => 'admin',
+                'authKey' => 'test100key',
+                'accessToken' => '100-token',
+            ],
+            '101' => [
+                'id' => '101',
+                'username' => 'demo',
+                'password' => 'demo',
+                'authKey' => 'test101key',
+                'accessToken' => '101-token',
+            ],
+            '102' => [
+                'id' => '102',
+                'username' => 'demo2',
+                'password' => 'demo2',
+                'authKey' => 'test102key',
+                'accessToken' => '102-token',
+            ],
+        ]";
     }
 }
